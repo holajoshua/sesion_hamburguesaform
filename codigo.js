e8bc69da3613e0540866ee2a26c5eff63dcdfdd9
holajoshua <118214300+holajoshua@users.noreply.github.com>
2023-11-26 15:01:24 -0500

    Add files via upload

---
+++ codigo.js
@@ -0,0 +1,11 @@
+window.addEventListener("DOMContentLoaded",(event)=>{
+    console.log("DOM completamente cargado y procesado");
+    const menuBtn=document.getElementById("menu");
+    const nav=document.querySelector("header nav");
+    const body=document.querySelector("body");
+    menuBtn.addEventListener("click",(event) =>{
+        menuBtn.classList.toggle("salir");
+        nav.classList.toggle("visible");
+        body.classList.toggle("no-scroll");
+    })
+})
---
+++ js/logicaNegocioUsuario.js
@@ -0,0 +1,26 @@
+// 1:admin;
+//  2:cliente;
+function obtenerListaUsuarios(){
+    var listaUsuarios=JSON.parse(localStorage.getItem('listaUsuariosLs'));
+    if (listaUsuarios==null) {
+        listaUsuarios=[
+              //id, nombre, apellido, correo contraseña ,fecha nac rol
+              ['1','Pabs','Monestel','pablo.monestel@gmail.com','1pablo9','1987-10-12','1'],
+              ['2','Olman','Santamaria','olman@gmail.com','1olman9','1993-04-17','2']
+        ]
+    }
+    return listaUsuarios;
+}
+function validarCredenciales(pCorreo , pContrasenna){
+    var listaUsuarios=obtenerListaUsuarios();
+    var bAcceso=false;
+    for (var i=0; i < listaUsuarios.length; i++) {
+        if(pCorreo==listaUsuarios[i][3] && pContrasenna==listaUsuarios[i][4]){
+            bAcceso=true;
+            sessionStorage.setItem('usuarioActivo',listaUsuarios[i][1]+' '+listaUsuarios[i][2]);
+            sessionStorage.setItem('rolUsuarioActivo',listaUsuarios[i][6]);
+        }
+        
+    }
+    return bAcceso;
+}
